c81471e4395e531c298f554d9072c7eb2b195f3c
Lego79 <kentcandoit89@gmail.com>
2022-11-18 23:19:34 +0900

    스태틱 연습

---
+++ src/main/java/ScopeAndStatic/staticPractice.java
@@ -0,0 +1,12 @@
+package ScopeAndStatic;
+
+public class staticPractice {
+
+
+        static int value = 10;
+        public static void main(String []args){
+            System.out.println(value);
+        }
+
+
+}
