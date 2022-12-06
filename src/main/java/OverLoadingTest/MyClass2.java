a5571eac67de1c064a90d414a5782b2b115b3176
Lego79 <kentcandoit89@gmail.com>
2022-12-06 21:10:09 +0900

    this 실습

--- src/main/java/OverLoadingTest/MyClass2.java
+++ src/main/java/OverLoadingTest/MyClass2.java
@@ -15,4 +15,6 @@ public class MyClass2 {
         return x + y;
     }
 
+
+
 }
---
+++ src/main/java/thisTest/ThisTest.java
@@ -0,0 +1,21 @@
+package thisTest;
+
+public class ThisTest {
+
+    String name;
+    int number;
+
+    public Car(){
+
+    }
+
+    public Car(String name){
+        this.name = name;
+    }
+
+    public Car(String name, int number){
+        this.name = name;
+        this.number = number;
+    }
+
+}
