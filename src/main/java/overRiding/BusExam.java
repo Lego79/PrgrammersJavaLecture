ac25e585ac4e848b4c0f2a51211c6e0f90c140cd
Lego79 <kentcandoit89@gmail.com>
2023-01-26 21:57:00 +0900

    오버라이딩 연습

---
+++ src/main/java/overRiding/BusExam.java
@@ -0,0 +1,10 @@
+package overRiding;
+
+public class BusExam {
+
+    public static void main(String args[]){
+        Bus bus = new Bus();
+        bus.run();  //Bus run메소드가 실행된다.
+    }
+
+}
---
+++ src/main/java/overRiding/OverRiding.java
@@ -0,0 +1,12 @@
+package overRiding;
+
+public class OverRiding {
+
+
+
+    public static void main(String[] args) {
+        Bus bus = new Bus();
+        bus.run();
+    }
+
+}
