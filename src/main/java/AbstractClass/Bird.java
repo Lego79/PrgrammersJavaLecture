2a8f4b3767f05aa0f7f60ce0f0ee90972acaa925
Lego79 <kentcandoit89@gmail.com>
2023-03-06 23:10:11 +0900

    선언 할당 시작

--- src/main/java/AbstractClass/Bird.java
+++ src/main/java/AbstractClass/Bird.java
@@ -2,10 +2,18 @@ package AbstractClass;
 
 public abstract class Bird {
 
-    public abstract void sing();
+    public abstract void sing(); //추상메서드 사용,
 
     public void fly() {
         System.out.println("날아");
     }
+
+    //variable
+
+//    int x; //declaration
+//
+//    x = 123; //assignment
+
+    int x = 123; // initializtion
 
 }
