b3216fabc3c9341f0b8b0ffdc11a1ee1987fb8bc
Lego79 <kentcandoit89@gmail.com>
2023-03-09 23:17:04 +0900

    12시간 자바 풀코스 스타트

--- src/main/java/TwelveHoursJavaCourse/Declaration/MathClass.java
+++ src/main/java/TwelveHoursJavaCourse/Declaration/MathClass.java
@@ -2,6 +2,27 @@ package TwelveHoursJavaCourse.Declaration;
 
 public class MathClass {
 
+    public static void main(String[] args) {
+
+
+        double x = 3.14;
+        double y = -10;
+
+        double z = Math.ceil(x);
+        //abs 절대값 -10 -> 10
+        //sqrt
+        //round  소수점 제거
+        // ceil 무조건 반올림
+        // floor 무조건 반내림
+
+
+        System.out.println(z);
+
+
+
+
+    }
+
 
 
 }
---
+++ src/main/java/TwelveHoursJavaCourse/RandomNumbers.java
@@ -0,0 +1,18 @@
+package TwelveHoursJavaCourse;
+
+import java.util.Random;
+
+public class RandomNumbers {
+
+    public static void main(String[] args) {
+
+        Random random = new Random();
+
+        boolean z = random.nextBoolean();
+
+        System.out.println(z);
+
+
+    }
+
+}
