0a7595b40a93bb4b5879339f83ffcfb208c2fa33
Lego79 <kentcandoit89@gmail.com>
2023-05-30 23:08:23 +0900

    List 공부

---
+++ src/main/java/arrayLiest/Biz.java
@@ -0,0 +1,38 @@
+package arrayLiest;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Biz {
+
+    public Biz() {
+    }
+
+    public List<Book> convert(String[] contents) {
+
+        List<Book> bookList = new ArrayList<>();
+
+
+        for(int i = 0 ; i < contents.length; i++) {
+
+            System.out.println(contents[i]);
+            String[] data = contents[i].split("/");
+            String category = data[0];
+            String title = data[1];
+            String author = data[2];
+            String publisher = data[3];
+            int price = Integer.parseInt(data[4]);
+            String isbn = data[5];
+            Book book = new Book(category, title, author, publisher, price, isbn);
+            bookList.add(book);
+
+
+
+
+        }
+
+        return bookList;
+
+    }
+
+}
---
+++ src/main/java/arrayLiest/BookStore.java
@@ -0,0 +1,32 @@
+package arrayLiest;
+
+import java.util.List;
+
+public class BookStore {
+
+    private List<Book> bookList;
+
+    public BookStore(List<Book> bookList) {
+        this.bookList = bookList;
+    }
+
+    public List<Book> getBookList() {
+        return bookList;
+    }
+
+    public void setBookList(List<Book> bookList) {
+        this.bookList = bookList;
+    }
+
+    public void printBookStore() {
+
+
+
+    };
+
+
+}
+
+
+
+
