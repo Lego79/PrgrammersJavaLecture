7d9e93564c70b3bb8fbcb7b9c99ee7dee53bb4ae
Lego79 <kentcandoit89@gmail.com>
2023-06-01 00:14:06 +0900

    어레이 리스트 및 스캐너 스터디

--- src/main/java/arrayLiest/App.java
+++ src/main/java/arrayLiest/App.java
@@ -2,27 +2,50 @@ package arrayLiest;
 
 import java.util.List;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.Reader;
+import java.util.List;
+
 public class App {
 
     public static void main(String[] args) {
 
-        String[] contents = {
-                "comic/열혈강호/전극진-양재현/대원씨아이/4500/979-11-5754-926-9",
+        String[] contents = {"comic/열혈강호/전극진-양재현/대원씨아이/4500/979-11-5754-926-9",
                 "travel/뉴욕 100배 즐기기/홍지윤/알에이치코리아/14400/978-89-255-8673-1",
                 "travel/바르셀로나 지금이 좋아/정다운/중앙북스/15000/978-89-991-7580-0",
                 "food/오늘의 맥주/이성준/오운/18000/979-11-92814-04-9",
-                "food/버번 위스키의 모든 것/조승원/(주)교유당/24000/979-11-90277-42-6"
-        };
+                "food/버번 위스키의 모든 것/조승원/(주)교유당/24000/979-11-90277-42-6"};
 
         Biz biz = new Biz();
         List<Book> bookList = biz.convert(contents);
 
-        for(int i = 0; i < bookList.size(); i++) {
-            System.out.println(i+1 + ".");
-            System.out.println(bookList.get(i).getTitle());
-        }
-
+        System.out.println("===== PrintBookStore() =====");
         BookStore bs = new BookStore(bookList);
+        bs.printBookStore();
+
+        System.out.println("===== PrintBookByCategory() =====");
+        String category = "travel";
+        bs.printBookStoreByCategory(category);
+
+        System.out.println("===== PrintBookByIsbn() =====");
+        String isbn = "979-11-90277-42-6";
+        bs.PrintBookByIsbn(isbn);
+
+        System.out.println("===== PrintBookByMaxPrice() =====");
+        int maxPrice = 10000;
+        bs.PrintBookByMaxPrice(maxPrice);
+
+        System.out.println("===== PrintBookByMinPrice() =====");
+        int minPrice = 15000;
+        bs.PrintBookByMinPrice(minPrice);
+
+        System.out.println("===== PrintBookByBetweenPrice() =====");
+        int ByBetweenMaxPrice = 5000;
+        int ByBetweenMinPrice = 20000;
+        bs.PrintBookByBetweenPrice(ByBetweenMinPrice, ByBetweenMaxPrice);
+
 
 
     }
--- src/main/java/arrayLiest/BookStore.java
+++ src/main/java/arrayLiest/BookStore.java
@@ -1,28 +1,115 @@
 package arrayLiest;
 
+import java.text.DecimalFormat;
 import java.util.List;
 
 public class BookStore {
 
     private List<Book> bookList;
 
-    public BookStore(List<Book> bookList) {
-        this.bookList = bookList;
+    public BookStore(List<Book> book) {
+        this.bookList = book;
     }
 
-    public List<Book> getBookList() {
+    public List<Book> getBook() {
         return bookList;
     }
 
-    public void setBookList(List<Book> bookList) {
-        this.bookList = bookList;
+    public void setBook(List<Book> book) {
+        this.bookList = book;
     }
 
     public void printBookStore() {
+        int count = 1;
+        for (int i = 0; i < bookList.size(); i++) {
+            System.out.println(count + ".");
+            bookList.get(i).printInfo();
+            count++;
 
+        }
 
+    }
 
-    };
+    public void printBookStoreByCategory(String category) {
+
+        int count = 1;
+        for (int i = 0; i < bookList.size(); i++) {
+
+            if (bookList.get(i).category.equals(category)) {
+                System.out.println(count + ".");
+                bookList.get(i).printInfo();
+                count++;
+
+            }
+
+        }
+
+    }
+
+    public void PrintBookByIsbn(String isbn) {
+
+        int count = 1;
+        for (int i = 0; i < bookList.size(); i++) {
+
+            if (bookList.get(i).isbn.equals(isbn)) {
+                System.out.println(count + ".");
+                bookList.get(i).printInfo();
+                count++;
+
+            }
+
+        }
+
+    }
+
+    public void PrintBookByMaxPrice(int maxPrice) {
+
+        int count = 1;
+        for (int i = 0; i < bookList.size(); i++) {
+
+            if (bookList.get(i).getPrice() < maxPrice) {
+                System.out.println(count + ".");
+                bookList.get(i).printInfo();
+                count++;
+
+            }
+
+        }
+
+    }
+
+    public void PrintBookByMinPrice(int minPrice) {
+
+        int count = 1;
+        for (int i = 0; i < bookList.size(); i++) {
+
+            if (bookList.get(i).getPrice() >= minPrice) {
+                System.out.println(count + ".");
+                bookList.get(i).printInfo();
+                count++;
+
+            }
+
+        }
+
+    }
+
+    public void PrintBookByBetweenPrice(int maxPrice, int minPrice) {
+
+        int count = 1;
+        for (int i = 0; i < bookList.size(); i++) {
+
+            if (bookList.get(i).getPrice() >= minPrice && bookList.get(i).getPrice() <= maxPrice ) {
+                System.out.println(count + ".");
+                bookList.get(i).printInfo();
+                count++;
+
+            }
+
+        }
+
+    }
+
 
 
 }
